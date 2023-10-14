25361d6a58332c4417a8eb762ae8a7f60b5044da
GabuTheDev <gabydev14@gmail.com>
2023-10-15 01:04:04 +0300

    handle both id & username for "creator" and "publisher" keywords
    + style (using prettier)

--- search.js
+++ search.js
@@ -1,8 +1,9 @@
 // declare regexes for keyword detection
 const regex = {
-	property: /creator:[a-zA-Z0-9]+|publisher:[a-zA-Z0-9]+|hd:(true|false)|sd:(true|false)|animated:(true|false)|extra:(true|false)|color:[a-zA-Z]+|nsfw:(true|false)/,
-	comparison: /(views|downloads|rating)([=<>]+)(\d+)$/
-}
+	property:
+		/creator:[a-zA-Z0-9]+|publisher:[a-zA-Z0-9]+|hd:(true|false)|sd:(true|false)|animated:(true|false)|extra:(true|false)|color:[a-zA-Z]+|nsfw:(true|false)/,
+	comparison: /(views|downloads|rating)([=<>]+)(\d+)$/,
+};
 
 // declare fuction for handling keywords
 const sortSkins = async (array, query, gamemode) => {
@@ -20,76 +21,85 @@ const sortSkins = async (array, query, gamemode) => {
 
 		// interate through terms and handle them individually
 		for (const term of terms) {
-
 			// check if term is a property keyword
 			if (term.match(regex.property)) {
-
 				// split term into key & value
-				let [key, value] = term.split(":"), exclude = false;
+				let [key, value] = term.split(":"),
+					exclude = false;
 
 				// check for exclusion
 				if (term.startsWith("-")) {
 					key = key.slice(1);
 
-					if (["creator", "publisher", "color"].includes(key)) exclude = true;
+					if (["creator", "publisher", "color"].includes(key))
+						exclude = true;
 					else value = !value;
 				}
 
+				//handle value type if keyword is "creator", "publisher";
+				if (["creator", "publisher"].includes(key)) {
+					!isNaN(value) ? (value = Number(value)) : null;
+					typeof value === "string"
+						? (value = value.toLowerCase())
+						: null;
+				}
+
 				// handle "creator" keyword
-				if (key === "creator") 
+				if (key === "creator")
 					result = array.filter((skin) =>
-						skin.creators["1"].some((creator) =>
+						skin.creators[
+							typeof value === "number" ? "0" : "1"
+						].some((creator) =>
 							exclude
-								? !creator.toLowerCase().includes(value.toLowerCase())
-								: creator.toLowerCase().includes(value.toLowerCase())
+								? !creator.toLowerCase().includes(value)
+								: creator.toLowerCase().includes(value),
 						),
 					);
-				
 				// handle "publisher" keyword (WIP)
 				else if (key === "publisher") continue;
-				
 				// handle "nsfw" keyword
 				else if (key === "nsfw")
 					result = value
 						? array.filter((skin) => skin._nsfw)
 						: array.filter((skin) => !skin._nsfw);
-				
 				// handle "sd" keyword
 				else if (key === "sd")
 					result = value
 						? array.filter((skin) => skin.files.includes(0))
 						: array.filter((skin) => !skin.files.includes(0));
-
 				// handle "hd" keyword
 				else if (key === "hd")
 					result = value
 						? array.filter((skin) => skin.files.includes(1))
 						: array.filter((skin) => !skin.files.includes(1));
-
 				// handle "animated" keyword
 				else if (key === "animated")
 					result = value
 						? array.filter((skin) => skin.files.includes(2))
 						: array.filter((skin) => !skin.files.includes(2));
-				
 				// handle "extra" keyword
 				else if (key === "extra")
 					result = value
 						? array.filter((skin) => skin.files.includes(3))
 						: array.filter((skin) => !skin.files.includes(3));
-
 				// handle "color" keyword (WIP)
 				else if (key === "color") continue;
 			} else if (term.match(regex.comparison)) {
-
 				// split term into key, comparison & value
-				let [_, key, comparison, value] = term.match(regex.comparison), exclude = false;
+				let [_, key, comparison, value] = term.match(regex.comparison),
+					exclude = false;
 
 				// force type "number" on value
 				value = Number(value);
 
 				// declare order of "stats" array
-				const order = ["views", "downloads", "likes", "dislikes", "rating"];
+				const order = [
+					"views",
+					"downloads",
+					"likes",
+					"dislikes",
+					"rating",
+				];
 
 				// declare comparation functions
 				const compare = {
@@ -97,18 +107,27 @@ const sortSkins = async (array, query, gamemode) => {
 					">=": (a, b) => a >= b,
 					"=": (a, b) => a === b,
 					"<=": (a, b) => a <= b,
-					"<": (a, b) => a < b
-				}
+					"<": (a, b) => a < b,
+				};
 
 				// handle exclusion
 				if (term.startsWith("-")) exclude = true;
+
 				result = array.filter((skin) =>
 					exclude
-						? !compare[comparison](skin.stats[order.indexOf(key)], value)
-						: compare[comparison](skin.stats[order.indexOf(key)], value)
+						? !compare[comparison](
+								skin.stats[order.indexOf(key)],
+								value,
+						  )
+						: compare[comparison](
+								skin.stats[order.indexOf(key)],
+								value,
+						  ),
 				);
 			}
 		}
 	}
+
+	// return filtered array
 	return result;
 };
